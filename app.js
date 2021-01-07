422db07aa9797b73e6f635323b3e96b8843db19a
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-01-07 15:06:25 -0800

    modified style.css

--- app.js
+++ app.js
@@ -1,64 +1,46 @@
-const addInput1 = document.getElementById('add-input-1');
-const addInput2 = document.getElementById('add-input-2');
-const addButton = document.getElementById('add-button');
-const addResults = document.getElementById('add-results');
+var inputOne = document.getElementById('input-1');
+var inputTwo = document.getElementById('input-2');
+var addButton = document.getElementById('add-button');
+var subButton = document.getElementById('sub-button');
+var multButton = document.getElementById('mult-button');
+var divButton = document.getElementById('div-button');
+var allResults = document.getElementById('results');
 
 
 
 addButton.addEventListener('click', () => {
-    const number1 = Number(addInput1.value);
-    const number2 = Number(addInput2.value);
+    const number1 = Number(inputOne.value);
+    const number2 = Number(inputTwo.value);
     const result = number1 + number2;
 
-    addResults.textContent = result;
+    allResults.textContent = result;
 }); 
-
-
-const subInput1 = document.getElementById('sub-input-1');
-const subInput2 = document.getElementById('sub-input-2');
-const subButton = document.getElementById('sub-button');
-const subResults = document.getElementById('sub-results');
-
-
 
 subButton.addEventListener('click', () => {
-    const number3 = Number(subInput1.value);
-    const number4 = Number(subInput2.value);
-    const result = number3 - number4;
+    const number1 = Number(inputOne.value);
+    const number2 = Number(inputTwo.value);
+    const result = number1 - number2;
 
-    subResults.textContent = result;
+    allResults.textContent = result;
 }); 
-
-
-const multInput1 = document.getElementById('mult-input-1');
-const multInput2 = document.getElementById('mult-input-2');
-const multButton = document.getElementById('mult-button');
-const multResults = document.getElementById('mult-results');
-
-
 
 multButton.addEventListener('click', () => {
-    const number5 = Number(multInput1.value);
-    const number6 = Number(multInput2.value);
-    const result = number5 * number6;
+    const number1 = Number(inputOne.value);
+    const number2 = Number(inputTwo.value);
+    const result = number1 * number2;
 
-    multResults.textContent = result;
+    allResults.textContent = result;
 }); 
-
-
-const divInput1 = document.getElementById('div-input-1');
-const divInput2 = document.getElementById('div-input-2');
-const divButton = document.getElementById('div-button');
-const divResults = document.getElementById('div-results');
-
-
 
 divButton.addEventListener('click', () => {
-    const number7 = Number(divInput1.value);
-    const number8 = Number(divInput2.value);
-    const result = number7 / number8;
+    const number1 = Number(inputOne.value);
+    const number2 = Number(inputTwo.value);
+    const result = number1 / number2;
 
-    divResults.textContent = result;
+    allResults.textContent = result;
+});
+ 
 
-}); 
+
+
 
